86a3fea461582f571339270cc9bad4f048660670
regycloud <regy888@gmail.com>
2021-03-30 00:52:16 +0700

    Adding elseifCondition file

---
+++ elseifCondition.js
@@ -0,0 +1,15 @@
+// Creating a program to keep track of the way the environment changes with the seasons.
+let season = 'summer';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') {
+  console.log('It\'s winter! Everything is covered in snow.');
+} else if (season === 'fall') {
+  console.log('It\'s fall! Leaves are falling!');
+} else if (season === 'summer') {
+  console.log('It\'s sunny and warm because it\'s summer!');
+} else {
+  console.log('Invalid season.');
+} 
+
